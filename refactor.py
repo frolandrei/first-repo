16ebc497dbc032971840c7009245d28f9eb7a341
four1squad <87648612+four1squad@users.noreply.github.com>
2023-11-11 16:43:30 +0300

    Update refactor.py
    Изменено:
    1. функция myBestFunctionToFindModule (проверка на отрицательность, название функции)
    2. запуск программы (__name__ == "__main__", разделители в массиве, цикл с проверкой равенства функции и модуля)

--- refactor.py
+++ refactor.py
@@ -1,11 +1,11 @@
-def myBestFunctionToFindModule:
-    if a < 0:
-        return -a
-    return a
+def myBestFunctionToFindModule(test):
+    if test[a] < 0:
+        return -test[a]
+    else:
+        return test[a]
 
 
-
-if __name__ = "__main__":
-    test = [-15 -1 0 1 15]
-    for test in test:
-        assert mybestfunctiontofindmodule(test) == abs(test)
+if __name__ == "__main__":
+    test = [-15, -1, 0, 1, 15]
+    for a in range(len(test)):
+        assert myBestFunctionToFindModule(test) == abs(test[a])
