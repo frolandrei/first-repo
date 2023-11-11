048344511281719c6f04802fb9bb95b71f177c7c
frolandrei <andreyyfrolov@yandex.ru>
2023-11-11 17:47:23 +0300

    Изменил тестирование

--- refactor.py
+++ refactor.py
@@ -1,10 +1,16 @@
-def my_abs(a: int | float):
-    if a < 0:
-        return -a
-    return a
+def my_abs(value: int | float):
+    if value < 0:
+        return -value
+    return value
 
 
 if __name__ == "__main__":
-    test = [-15, -1, 0, 1, 15]
-    for a in test:
-        assert my_abs(a) == abs(a)
+    bad_tests = 0
+    if my_abs(-1) != 1: bad_tests += 1
+    if my_abs(0)  != 0: bad_tests += 1
+    if my_abs(1)  != 1: bad_tests += 1
+    if my_abs(-1.0) != 1.0: bad_tests += 1
+    if my_abs(0.0)  != 0.0: bad_tests += 1
+    if str(my_abs(-0.0))  != str(0.0): bad_tests += 1
+    print(f"Количество непройденных тестов: {bad_tests}")
+
