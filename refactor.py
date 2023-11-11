04df50f5364644b980cf14011c5aed7d1245b77a
four1squad <87648612+four1squad@users.noreply.github.com>
2023-11-11 17:13:32 +0300

    Update refactor.py
    Изменено:
    1. TypeHint в функции, принимается значение, а не контейнер, нет else
    2. Цикл, проверка

--- refactor.py
+++ refactor.py
@@ -1,11 +1,10 @@
-def myBestFunctionToFindModule:
+def myBestFunctionToFindModule(a: int | float):
     if a < 0:
         return -a
     return a
 
 
-
-if __name__ = "__main__":
-    test = [-15 -1 0 1 15]
-    for test in test:
-        assert mybestfunctiontofindmodule(test) == abs(test)
+if __name__ == "__main__":
+    test = [-15, -1, 0, 1, 15]
+    for a in test:
+        assert myBestFunctionToFindModule(a) == abs(a)
