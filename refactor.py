3e5add9c879ffc407983fec400a988fc20b4ea90
frolandrei <andreyyfrolov@yandex.ru>
2023-11-11 17:39:23 +0300

    Изменил название функции на более простое

--- refactor.py
+++ refactor.py
@@ -1,4 +1,4 @@
-def myBestFunctionToFindModule(a: int | float):
+def my_abs(a: int | float):
     if a < 0:
         return -a
     return a
@@ -7,4 +7,4 @@ def myBestFunctionToFindModule(a: int | float):
 if __name__ == "__main__":
     test = [-15, -1, 0, 1, 15]
     for a in test:
-        assert myBestFunctionToFindModule(a) == abs(a)
+        assert my_abs(a) == abs(a)
