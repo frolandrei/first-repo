26d8d045a463619ede5cf4802d224e5197a2f6e3
Teutonic Knight <63908131+Bad-6666@users.noreply.github.com>
2023-11-11 17:59:44 +0300

    Bad 6666 patch refactor (#7)
    * Updatexxx refactor.cpp
    
    update
    
    * Update refactor.cpp

--- refactor.cpp
+++ refactor.cpp
@@ -1,16 +1,26 @@
-double myBestFunctionToFindModuleOfADoubleNumber(double a)
+#include <iostream>
+#include <vector>
+
+double bestMod(double a)
 {
-  if (a > 0)
-    return a
-  return -a;
+  if (a >= 0) {
+    return a; 
+      } else {
+            double b=a-(2*a);
+            return b;
+  }
 }
 
 int main()
 {
-  std::vector test_cases = -1.0, -0.5, 0.0, 0.5, 1.0;
-  for (auto test_case : test_cases)
-    {
-      if (myBestFunctionToFindModuleOfADoubleNumber(test_case) != abs(test_case))
-        std::cout << "Тест не пройден " << test_case << std::endl;
+  std::vector<double> test_cases = {-1.0, -0.5, 0.0, 0.5, 1.0};
+  int xyzw=0;
+  while (xyzw < test_cases.size()) {
+      if (bestMod(test_cases[xyzw]) != abs(test_cases[xyzw])) {
+          std::cout << "Тест не пройден " << test_cases[xyzw] << "   ";
+      } else { 
+          std::cout << bestMod(test_cases[xyzw]) << "  ";
     }
+    xyzw=xyzw+1;
+}
 }
